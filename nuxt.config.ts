a19e88a4d050afac1c6c7b44eb1ef45c3d99a1b7
Mai Quang Linh <mai.quang.linh.hl97@gmail.com>
2023-05-08 21:49:59 +0900

    add i18n + ant

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     // ...
     '@pinia/nuxt',
   ],
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
+    ],
+    ssr: {
+      noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue','@ant-design/icons-vue', 'lodash-es', /vue-i18n/]
+    },
+  }
 });
